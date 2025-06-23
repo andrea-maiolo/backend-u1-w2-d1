1aa3553179646e7d27abe211f6a3b09ec79e37da
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-06-23 15:24:54 +0200

    added ex2

--- src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // private static Logger logger = LoggerFactory.getLogger(Main.class);
-    }
-}
---
+++ src/ex2/Main.java
@@ -0,0 +1,54 @@
+package ex2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //  consumoMotore();
+        consumoMotoreDouble();
+
+    }
+
+    public static void consumoMotore() {
+        Scanner scanner = new Scanner(System.in);
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        try {
+
+            logger.info("dammi il numero di litri consumati ");
+            int litri = Integer.parseInt(scanner.nextLine());
+            logger.info("dammi il numero di kilometri fatti");
+            int km = Integer.parseInt(scanner.nextLine());
+            int consumo = km / litri;
+            System.out.println("il tu motore consuma " + consumo);
+        } catch (ArithmeticException ex) {
+            logger.error("non puoi esserti mosso senza benzina");
+        } catch (NumberFormatException ex) {
+            logger.error("per favore passa un numero");
+        }
+        ;
+
+    }
+
+    public static void consumoMotoreDouble() {
+        Scanner scanner = new Scanner(System.in);
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        try {
+            logger.info("dammi il numero di litri consumati ");
+            double litri = Double.parseDouble(scanner.nextLine());
+            logger.info("dammi il numero di kilometri fatti");
+            double km = Double.parseDouble(scanner.nextLine());
+            double consumo = km / litri;
+            System.out.println("il tu motore fa " + consumo + " kilometri ogni litro");
+        } catch (ArithmeticException ex) {
+            System.out.println("non puoi esserti mosso senza benzina");
+        } catch (NumberFormatException ex) {
+            logger.info("per favore passa un numero");
+        }
+        ;
+
+    }
+}
