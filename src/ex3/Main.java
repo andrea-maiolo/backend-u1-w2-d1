a2d7da8e1dff150a7ecd60a9d491aae56f07b220
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-06-23 17:54:57 +0200

    minor cahnge

--- src/ex3/Main.java
+++ src/ex3/Main.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
-        ContoCorrente myConto = new ContoCorrente("topolinio", 240.6);
+        ContoCorrente myConto = new ContoCorrente("topolino", 240.6);
 
         try {
             myConto.preleva(100.50);
